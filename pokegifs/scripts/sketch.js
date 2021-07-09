a93741c98bbe2ea1e487676f6a4bc3591563d16b
ellis <david.ellis@uni-goettingen.de>
2021-07-09 11:38:01 +0200

    Added basic page for finding pokemon gifs

---
+++ pokegifs/scripts/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+	// needed in order to use p5.js
+	noCanvas()
+}
+
+function get_gif() {
+		poke_name = document.getElementById("input").value
+		console.log(poke_name)
+		if (poke_name != "Enter Pokémon name/id"){
+			url = `https://pokeapi.co/api/v2/pokemon/${poke_name}`
+			pokeData = loadJSON(url, displayGif);
+		}
+}
+
+function clearOutput() {
+	// If gif_box div exists - remove it.
+	if (document.getElementById("gif_box")) {
+		outDiv = document.getElementById("gif_box");
+		outDiv.remove()
+	}	
+}
+
+function displayGif(){
+	// As test just display the first 1
+	clearOutput()
+	
+	let outDiv = createDiv();
+	outDiv.id("gif_box")
+	outDiv.parent(id = "mainBox")
+	//output.load(pokeData)
+	
+	let gif_box = document.getElementById("gif_box");
+	let gif_url = pokeData["sprites"]["versions"]["generation-v"]["black-white"]["animated"]["front_default"];
+	
+	var img = document.createElement("img");
+	img.src = gif_url;
+	var src = document.getElementById("gif_box");
+	src.appendChild(img);
+	
+}
