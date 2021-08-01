f996dc829dd13baee409116cb24e97e979fe0bfb
ellis <david.ellis@uni-goettingen.de>
2021-08-01 18:02:13 +0200

    made homepage with nav links on all sub pages. much css. made weather app more useful

---
+++ scripts/main-script.js
@@ -0,0 +1,20 @@
+//------------------------------  Nav-bar ----------------------------------//
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(dropID) {
+    console.log(dropID)
+    document.getElementById(dropID).style.display = "block";
+    }
+    
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(e) {
+    if (!e.target.matches('#button1')) {
+        document.getElementById("drop1").style.display = "none";
+    }
+    if (!e.target.matches('#button2')) {
+        document.getElementById("drop2").style.display = "none";
+    }
+    }
+
+//--------------------------------------------------------------------------//
