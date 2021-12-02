8ce737fd00e36db49fe831141133a1b80faf8870
David Ellis <david.ellis94@gmail.com>
2021-12-02 13:11:05 +0100

    added postcode search functionality and basic styling

--- mini-projects/ucu-strike/sketch.js
+++ mini-projects/ucu-strike/sketch.js
@@ -1,9 +1,9 @@
 let postcodeData;
-let weatherData;
 let validate;
 let postcode;
-
-
+let jsonData
+var longs = [];
+var lats = [];
 
 function setup() {
 	noCanvas()
@@ -14,8 +14,9 @@ window.addEventListener("load", function () {
 	const pattern = /#postcode_/;
 	let pageURL = document.location.href
 
-	const jsonData=loadJSON('unis_with_locs.json'); 
-	console.log(jsonData);
+	jsonData=loadJSON('https://david-ellis.github.io/mini-projects/ucu-strike/unis_with_locs.json'); 
+	
+
 
 	if (pattern.test(pageURL)){
 		let n = pageURL.search("#postcode_");
@@ -52,7 +53,7 @@ function checkData() {
 		// Change url so this can be saved
 		changeURL(postcode) 
 
-		postcodeData = loadJSON(url, getWeatherWithPostcode);
+		postcodeData = loadJSON(url, getNearestPicket);
 	} else {
 		clearOutput()
 		para = createP("Invalid postcode :(")
@@ -64,14 +65,14 @@ function checkData() {
 function changeURL(postcode) {
 	// adds postcode to end of url
 	let	pageURL = document.location.href;
-	let n = pageURL.search("/weather.html");
-	document.location = pageURL.slice(0, n+13) +"#postcode_" + postcode;
+	let n = pageURL.search("/find-a-picket.html");
+	document.location = pageURL.slice(0, n+19) +"#postcode_" + postcode;
 }
 
 function clearOutput() {
-	// If outputWeather div exists - remove it.
-	if (document.getElementById("outputWeather")) {
-		outDiv = document.getElementById("outputWeather");
+	// If outputInfo div exists - remove it.
+	if (document.getElementById("outputInfo")) {
+		outDiv = document.getElementById("outputInfo");
 		outDiv.remove()
 	}
 	// If invalid input p exists - remove it.
@@ -81,102 +82,96 @@ function clearOutput() {
 	}
 }
 
-function getWeatherWithPostcode() {
+function getCoords(){
+	let longs = []
+	let lats = []
+	for (var i in jsonData["Longitude"]){
+		longs.push(jsonData["Longitude"][i])
+		lats.push(jsonData["Latitude"][i])
+	}
+
+	return [longs, lats];
+}
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+	var R = 6371; // Radius of the earth in km
+	var dLat = deg2rad(lat2-lat1);  // deg2rad below
+	var dLon = deg2rad(lon2-lon1); 
+	var a = 
+	  Math.sin(dLat/2) * Math.sin(dLat/2) +
+	  Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	  Math.sin(dLon/2) * Math.sin(dLon/2)
+	  ; 
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c; // Distance in km
+	return d;
+  }
+
+function deg2rad(deg) {
+	return deg * (Math.PI/180)
+}
+
+function getAllDistances(long, lat, longs, lats) {
+	let dists = [];
+	let dist_i;
+
+	for (var i in longs){
+		dist_i = getDistanceFromLatLonInKm(lats[i],longs[i], lat, long)
+		dists.push(dist_i)
+	}
+	return dists
+}
+
+function nearestIndex(long, lat){
+	coords = getCoords()
+	longs = coords[0]
+	lats = coords[1]
+	
+	dists = getAllDistances(long, lat, longs, lats)
+	minDist = Math.min(...dists)
+	return dists.indexOf(minDist)
+}
+
+function getNearestPicket() {
 	lat = postcodeData.result.latitude;
 	long = postcodeData.result.longitude
 	
-	api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=metric&exclude=current,minutely,hourly&appid=${weatherKey}`;
-	
 	clearOutput()
 	let outDiv = createDiv();
-	outDiv.id("outputWeather")
+	outDiv.id("outputInfo")
 	outDiv.parent(id = "mainBox")
-	
-	// Add subtitle
-	townTitle = createElement('h2', postcodeData.result.admin_district);
-	townTitle.id("townTitle")
-	townTitle.parent(outDiv)
-	
-	weatherData = loadJSON(api, displayWeather);
+
+	index = nearestIndex(long, lat)
+
+	displayInfo(index)
 }
 
-function displayWeather() {
+function makeTable(parent_id, index) {
+	const info_items = ["Name", "Postcode", "Branch website", "Branch twitter", "Branch facebook"]
+	table = createElement('table');
+	table.parent(parent_id)
+	for (var i in info_items){
+		info_data = jsonData[info_items[i]][String(index)]
+		if (info_data != null){
+			row = createElement('tr')
+			row.parent(table)
 
+			header = createElement('th', info_items[i])
+			header.parent(row)
+			info = createElement('td', info_data)
+			info.parent(row)
+		}
 
-	div = createDiv();
-	div.parent(id = "outputWeather")
-	div.id("weatherBoxes")
-	for (i=0; i<5; ++i) {
-		div = createDiv();
-		div.parent(id = "weatherBoxes")
-		
-		weekdayHeading = createElement('h3', getWeekDay(i));
-		weekdayHeading.parent(div)
-		
-		//console.log(weatherData	)
-		mainWeather = weatherData.daily[i].weather[0].main;
-		mainEmoji = getWeatherEmoji(mainWeather);
-		mainEmoji.parent(div)
-		mainEmoji.id("weatherEmoji")
-		
-		weatherDesc = weatherData.daily[i].weather[0].description;
-		weatherTxt = createP(weatherDesc)
-		weatherTxt.parent(div)
-		weatherTxt.id("outputDesc")
-		
-		temp = weatherData.daily[i].temp.day;
-		tempTxt = createP(Math.round(temp) + "&#176;C")
-		tempTxt.parent(div)
-		tempTxt.id("temp")
-		
-		
-	}
+	}	
+
+	return table
 }
 
-function getWeatherEmoji(mainWeather) {
-	//console.log("mainWeather")
-	if (mainWeather === "Rain") {
-		para = createP("☔");
-	}
-	else if (mainWeather === "Clear"){
-		para = createP("☀️");
-	}
-	else if (mainWeather === "Clouds"){
-		para = createP("☁️");
-	}
-	else if (mainWeather === "Thunderstorm"){
-		para = createP("🌩️");
-	}
-	else if (mainWeather === "Snow"){
-		para = createP("🌨️");
-	}
-	else if (mainWeather === "Drizzle"){
-		para = createP("🌦️");
-	}
-	else if (mainWeather === "Mist"){
-		para = createP("🌫️")
-	}
-	else {
-		para = createP(mainWeather);
-	}
-	return para
+function displayInfo(index) {
+	div = createDiv()
+	div.id("picketInfo")
+	div.parent("outputInfo")
+
+	table = makeTable("outputInfo", index) 
+	
 }
-
-function getWeekDay(fromToday) {
-	// Returns weekday <fromToday> days away from today
-	var d = new Date();
-	var weekdays = ["Sunday","Monday", "Tuesday","Wednesday","Thursday", "Friday","Saturday"];
-	
-	var index = d.getDay() + fromToday;
-	
-	if (index >= 7) {
-		index -= 7;
-	}
-	
-	weekday = weekdays[index]
-	
-	return weekday
-}
-
-
-
